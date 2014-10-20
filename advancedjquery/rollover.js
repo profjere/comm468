cdbb68baf3f1ca8b4adb5b22dc6696d87e060544
Jeremy Sarachan <jsarachan@sjfc.edu>
2014-10-20 18:21:11 -0400

    Create rollover.js

---
+++ advancedjquery/rollover.js
@@ -0,0 +1,10 @@
+$(function(){	
+	$('#adspace').hover(
+		function(){
+			$('#adspace').css('background-image','url("images/shoe_over.jpg")');
+		},
+		function(){
+			$('#adspace').css('background-image','url("images/shoe.jpg")');
+	
+  });
+});
